6faacf022d1504227de3f8db8fa15686c7fc92fa
Luciano Mammino <lucianomammino@gmail.com>
2016-05-10 21:42:22 +0100

    Added linter. Closes #5

--- index.js
+++ index.js
@@ -1,72 +1,71 @@
 #!/usr/bin/env node
 
-"use strict";
+'use strict';
 
 const fs = require('fs');
 const path = require('path');
 const request = require('request');
-const config = require('./config');
+// const config = require('./config');
 
 const lastIpFile = path.join(__dirname, '.lastip');
 
 function getCurrentIp() {
-  return new Promise((resolve, reject) => {
-    request('https://api.ipify.org/', (err, response, ip) => {
-      if (err) {
-        return reject(err);
-      }
+	return new Promise((resolve, reject) => {
+		request('https://api.ipify.org/', (err, response, ip) => {
+			if (err) {
+				return reject(err);
+			}
 
-      resolve(ip);
-    });
-  });
+			resolve(ip);
+		});
+	});
 }
 
 function getLastIp() {
-  return new Promise((resolve, reject) => {
-    if (!fs.existsSync(lastIpFile)) {
-      return resolve(undefined);
-    }
+	return new Promise((resolve, reject) => {
+		if (!fs.existsSync(lastIpFile)) {
+			return resolve(undefined);
+		}
 
-    fs.readFile(lastIpFile, 'utf8', (err, ip) => {
-      if (err) {
-        return reject(err);
-      }
+		fs.readFile(lastIpFile, 'utf8', (err, ip) => {
+			if (err) {
+				return reject(err);
+			}
 
-      resolve(ip);
-    });
-  });
+			resolve(ip);
+		});
+	});
 }
 
 function saveLastIp(ip) {
-  return new Promise((resolve, reject) => {
-    fs.writeFile(lastIpFile, ip, 'utf8', (err) => {
-      if (err) {
-        return reject(err);
-      }
+	return new Promise((resolve, reject) => {
+		fs.writeFile(lastIpFile, ip, 'utf8', (err) => {
+			if (err) {
+				return reject(err);
+			}
 
-      resolve();
-    });
-  }); 
+			resolve();
+		});
+	});
 }
 
-function updateRecords(){
-  console.log('Should update records now...');
+function updateRecords() {
+	console.log('Should update records now...');
 }
 
 let lastIp;
 let currentIp;
 
 getLastIp()
-  .then((ip) => {
-    lastIp = ip;
-    return getCurrentIp(); 
-  })
-  .then((ip) => {
-    currentIp = ip;
-    if (lastIp !== currentIp) {
-      saveLastIp(currentIp).then(updateRecords);
-    }
-  })
-  .catch(console.error)
+.then((ip) => {
+	lastIp = ip;
+	return getCurrentIp();
+})
+.then((ip) => {
+	currentIp = ip;
+	if (lastIp !== currentIp) {
+		saveLastIp(currentIp).then(updateRecords);
+	}
+})
+.catch(console.error)
 ;
-
